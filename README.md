c02b3b4260dd7f1eb3195d9bb476ab57846b6c05
Alexander Tsoy <Lils2010@gmail.com>
2019-01-23 00:34:55 +0300

    added tests and README

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# rest_translator
+# RestTranslator
+REST-api for text translation using Yandex.translate
+
+## Requirements
+* Java 8
+* Maven 3+
+
+## Usage
+Build application and run integration tests with:
+```
+    mvn clean package
+```
+Then execute the application with:
+```
+    java -jar target/rest-translator-0.0.1-SNAPSHOT.jar
+```
+The application runs on port 8080
+
+## Endpoints
+### Translate
+Request:
+```
+    GET localhost:8080/translate?text=один два три&to=en&from=ru
+```
+Response:
+```
+    HTTP 200 OK
+    [ "one", "three", "two" ]
+```
+Parameter "from" is optional
+
+### Get log
+Request:
+```
+    GET localhost:8080/log
+```
+Response:
+```
+    HTTP 200 OK
+    [ {
+      "id" : 1,
+      "dateTime" : "2019-01-23T00:31:24.112+03:00",
+      "inputText" : "один два три",
+      "translatedText" : "one three two",
+      "lang" : "ru-en",
+      "status" : "SUCCESS"
+    } ]
+```
--- src/test/java/tsoy/alexander/resttranslator/RestTranslatorApplicationTests.java
+++ src/test/java/tsoy/alexander/resttranslator/RestTranslatorApplicationTests.java
@@ -1,17 +1,52 @@
 package tsoy.alexander.resttranslator;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.JSONArray;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class RestTranslatorApplicationTests {
 
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
     @Test
-    public void contextLoads() {
+    public void translate() throws Exception {
+        String response = restTemplate.getForObject("http://localhost:" + port +
+                "/translate?text=один два три четыре пять шесть семь&to=en", String.class);
+        ObjectMapper mapper = new ObjectMapper();
+        List<String> list = mapper.readValue(response,
+                new TypeReference<ArrayList<String>>() {
+                });
+        assertTrue(list.containsAll(Arrays.asList("six", "seven", "two", "five", "four", "one", "three")));
     }
 
+    @Test
+    public void translateFromSpanishToEnglish() throws Exception {
+        String response = restTemplate.getForObject("http://localhost:" + port +
+                "/translate?text=siete cinco una dos seis cuatro tres&to=en&from=es", String.class);
+        ObjectMapper mapper = new ObjectMapper();
+        List<String> list = mapper.readValue(response,
+                new TypeReference<ArrayList<String>>() {
+                });
+        assertTrue(list.containsAll(Arrays.asList("six", "seven", "two", "five", "four", "one", "three")));
+    }
 }
-
